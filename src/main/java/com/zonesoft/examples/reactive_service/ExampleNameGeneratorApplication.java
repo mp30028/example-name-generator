8eb3984f16bdfbc5a30bd2a8ac06ec400130e6a3
mp30028 <mp30028@gmail.com>
2022-03-20 17:15:57 +0000

    initial commit

---
+++ src/main/java/com/zonesoft/examples/reactive_service/ExampleNameGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.zonesoft.examples.reactive_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleNameGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleNameGeneratorApplication.class, args);
+	}
+}
